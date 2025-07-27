bc708e8570efbda65cc47c7c7a1f4da420957f93
Gohul.R <115005034+BEgohul@users.noreply.github.com>
2025-07-27 21:27:02 +0530

    multiple parameter

---
+++ method.java
@@ -0,0 +1,14 @@
+package newjava;
+
+public class method {
+    //multiple parameter
+    static void mymethod(String name, int age){
+        System.out.println(name + " " + age);
+    }
+
+    public static void main(String[] args) {
+        mymethod("gohul", 21);
+        mymethod("ravi", 11);
+        mymethod("krish", 10);
+    }
+}
