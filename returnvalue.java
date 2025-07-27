f1ab074a0c13b944eb404cedf7f0daec5160fbb5
Gohul.R <115005034+BEgohul@users.noreply.github.com>
2025-07-27 22:25:15 +0530

    return value

---
+++ returnvalue.java
@@ -0,0 +1,11 @@
+package newjava;
+
+public class returnvalue {
+    static int mymethod(int a, int b){
+    return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mymethod(23,98612));
+    }
+}
