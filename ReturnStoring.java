122ed7d264e9408e0f38204de4772e0d3ffc13d4
Gohul.R <115005034+BEgohul@users.noreply.github.com>
2025-07-27 22:33:22 +0530

    it returns value that stored in separate method

---
+++ ReturnStoring.java
@@ -0,0 +1,12 @@
+package newjava;
+
+public class ReturnStoring {
+    static int mymethod(int a , int b){
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        int z = mymethod(908324, 9871298);
+        System.out.println(z);
+    }
+}
