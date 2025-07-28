dc5473a2a6ec03907d832391fe93923d6b4025ad
Gohul.R <115005034+BEgohul@users.noreply.github.com>
2025-07-28 23:33:45 +0530

    Functions
    Arrays using Functions

---
+++ Functions.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Functions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[4];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(in.next());
+        }
+            System.out.println(Arrays.toString(arr));
+
+            change(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+        static void change ( int[] arr){
+            arr[0] = 99;
+        }
+    }
+
