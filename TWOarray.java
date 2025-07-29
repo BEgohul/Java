86747798aa6403fe8c666a9459ec5b9fee884708
Gohul.R <115005034+BEgohul@users.noreply.github.com>
2025-07-29 23:29:19 +0530

    Add files via upload

--- TWOarray.java
+++ TWOarray.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
-package newjava;
 
 public class TWOarray {
     public static void main(String[] args) {
-        int[][] arr= new int[3][3];
+
 //        int[][] arr = {
 //                {1,2,3},
 //                {4,5},
 //                {6,7,8,9}
 //        };
         Scanner in = new Scanner(System.in);
+        int[][] arr= new int[3][3];
         System.out.println(arr.length ); //no. of rows
         //input
-        for(int row = 0 ; row < arr.length ; row++){
-
-            for(int col = 0 ; col < arr[row]) ; col++){
-            arr[row][col] = in.nextInt();
+        for(int row = 0 ; row < arr.length ; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
 
         }
+        //output
+               for (int row = 0 ; row < arr.length ; row++){
+                    for(int col = 0 ; col < arr[row].length ; col++){
+                System.out.println(arr[row][col] + "");
+
+        }
+            System.out.println();
 
     }
 }
